47fb6035d1700ed880f9a1d71eb1cd6de8a8a428
Asifyousaf <114678772+Asifyousaf@users.noreply.github.com>
2023-05-31 20:59:04 +0400

    Commit

--- script.js
+++
@@ -1,6 +0,0 @@
-//Adding  this script tag after the splash screen content 
-  // Hide the splash screen after 3 seconds 
-  setTimeout(function() {
-    document.getElementById('Splash').style.display = 'none';
-  }, 000);
-// 3 sec timer
